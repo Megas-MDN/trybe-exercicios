64a193df356dc9157e984b68a0319211a6944b1b
Megas-MDN <mdn.melqui@gmail.com>
2022-07-21 00:26:36 -0300

    Terminando todos os Bonus do dia 3.

--- fundamentos/b4-javaScript-intro/script3.js
+++ fundamentos/b4-javaScript-intro/script3.js
@@ -80,4 +80,179 @@ function maxPrimo(n){
 }
 
 let number = 14;
-maxPrimo(number);
+//maxPrimo(number);
+// Bonus 1
+function astQuadrado(n){
+    let c = '';
+    for(let i = 0; i<n ; i+=1){
+        c+='*';
+    }
+    for(let i = 0; i<n ; i+=1){
+        console.log(c);
+    }
+}
+
+//astQuadrado(5);
+
+//Bonus 2
+function triAst1(n){
+    let c = '*';
+    for(let i = 0; i< n; i+=1){
+        console.log(c);
+        c+='*'; 
+    }
+
+}
+
+//triAst1(8);
+//Bonus 3
+
+function triAst2(n){
+    let c = '';
+    let l='';
+    for(let j = 0; j< n;j+=1){
+        for(let i = j+1; i<n; i+=1){
+            c+=' ';
+        }
+          l+='*';
+          console.log(c+l);
+          c = '';
+    }
+
+
+}
+//triAst2(4)
+// Bonus 4
+
+//Resolve N de uma PA para calcular as camadas da piramide
+function numLoops(n){
+    let nloop = 0;
+    if((n - 1) % 2 === 0){
+        nloop = ((n - 1)/2) + 1;
+            return nloop;
+       } else if((n - 2) % 2 === 0 && (n - 2) >= 2){
+           nloop = ((n - 2)/2) + 1;
+           return nloop;
+       } else{
+           return 'fail';
+       }
+}
+
+
+function triAst3(n){
+    let nloop = numLoops(n);
+    
+    // resolvendo uma PA para saber quantos loops terá
+    //console.log("Numero de loops :: " + nloop);
+
+    let c = '';
+    let l='';
+    let c2 = '*';
+    for(let j = 0; j< nloop;j+=1){
+        for(let i = j+1; i<nloop; i+=1){
+            c+=' ';
+        }
+          l+='*';
+          //console.log(c+l);
+          if(j!==0){ 
+          console.log(c+l+c2);
+            c2+='*'; 
+          }else{
+            console.log(c+l);
+          }
+          c = '';
+    }
+
+
+    
+}
+
+//triAst3(11)
+/*
+*
+
+x*
+** * 3 - 1 
+
+xx*
+x** *
+*** ** 5 - 2 
+
+xxx*
+xx** *
+x*** **
+**** *** 7 - 3  
+
+xxxx*
+xxx** *
+xx*** **
+x**** ***
+***** **** 9 - 4 
+
+
+*/
+//Bonus 5
+
+
+function triAst4(n){
+    let nloop = numLoops(n);
+    let c = '';
+    let v = '';
+    let x = '*';
+
+    for(let i=0; i<nloop-1;i+=1){
+        c = '';
+        for(let j=i+1;j<nloop;j+=1){
+            c+=' ';
+        }
+        if(i === 0){
+            console.log(c+'*');
+        }else{
+            
+            console.log(c+'*'+v+x);
+            v+=' ';
+            
+
+        }
+        v+=' ';
+        
+    }
+    c = '';
+    for(let i = 0; i<n;i+=1){
+        c+='*';
+    }
+    console.log(c);
+
+}
+//triAst4(7)
+
+
+
+//Bonus 6 
+function isPrimo(n){
+    if(primo(n) === 1){
+        console.log("É primo ::: " + n);
+    } else if(primo(n) === -1){
+        console.log("NAO é primo ::: " + n)
+    }
+    else{
+        console.log("Falha ... número não foi reconhecido");
+        return 'erro';
+    }
+}
+let num1 = 19;
+//isPrimo(num1);
+
+
+/*
+
+ 
+xxxx*
+xxx*x *
+xx*xx x*
+x*xxx xx*
+
+***** ****
+
+
+*/
