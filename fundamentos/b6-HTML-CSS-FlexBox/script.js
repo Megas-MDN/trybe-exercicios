dc9790c8c65549b6f154000859988e65c34057e0
Megas-MDN <mdn.melqui@gmail.com>
2022-08-04 20:04:25 -0300

    Fazendo form rustico

--- fundamentos/b6-HTML-CSS-FlexBox/script.js
+++ fundamentos/b6-HTML-CSS-FlexBox/script.js
@@ -1,16 +1,32 @@
-// SELETORES
-const INPUT_TEXT = document.querySelector("#input-text");
-const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
-const HREF_LINK = document.querySelector("#href");
+// // SELETORES
+// const INPUT_TEXT = document.querySelector("#input-text");
+// const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
+// const HREF_LINK = document.querySelector("#href");
 
-function noneDefault(e) {
-    e.preventDefault();
-}
+// function noneDefault(e) {
+//     e.preventDefault();
+// }
 
-HREF_LINK.addEventListener('click', noneDefault);
-INPUT_CHECKBOX.addEventListener('click', noneDefault);
-INPUT_TEXT.addEventListener('keypress', (e) => {
-    if(e.key !== 'a') {
-        noneDefault(e);
+// HREF_LINK.addEventListener('click', noneDefault);
+// INPUT_CHECKBOX.addEventListener('click', noneDefault);
+// INPUT_TEXT.addEventListener('keypress', (e) => {
+//     if(e.key !== 'a') {
+//         noneDefault(e);
+//     }
+// });
+const sandy = document.getElementById('send');
+const term = document.getElementById('termos');
+
+term.addEventListener('click', (e) => {
+    if(e.target.checked){
+        //console.log('habilitou');
+        sandy.disabled = false;
+    } else {
+        //console.log('desabilitou');
+        sandy.disabled = true;
     }
 });
+
+sandy.addEventListener('click', (e) => {
+    //e.preventDefault();
+})
